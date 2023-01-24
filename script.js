c9f581e2712cf75fe57cb50c0c6996777f9aba84
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-24 11:57:53 +1100

    Create book elements and append to DOM

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let addBookForm = document.querySelector("#add-book-form");
 let pageBody = document.querySelector("body");
 let escapeMenuButton = document.querySelector('#escape-menu-button');
 let submitFormButton = document.querySelector('#submit-form-button');
+let myLibrary = [];
+let mainContentDiv = document.querySelector('.main-content');
 
 //Shorthand function that removes and adds specific classes for an element
 function addRemoveClass(element, removedClass, addedClass) {
@@ -23,31 +25,58 @@ escapeMenuButton.addEventListener('click', () => {
     addBookForm.reset();
 });
 
-let myLibrary = [];
-
 function Book(author, title, pages) {
     this.author = author;
     this.title = title;
     this.pages = pages;
 };
 
-function addBookToLibrary(book) {
+function addBookToLibrary(author, title, pages) {
+    let book = new Book(`${author}`, `${title}`, pages);
     myLibrary.push(book);
+    createBookDisplay(book);
     console.log(myLibrary);
 };
 
-function createNewBook(author, title, pages) {
-    let book = new Book(`${author}`, `${title}`, pages);
-    addBookToLibrary(book);
-};
-
 addBookForm.addEventListener('submit', (event) => {
+    //Stop form from actually submitting and turning the data into an object
     event.preventDefault();
     let data = Object.fromEntries(new FormData(event.target).entries());
-    createNewBook(data.author, data.title, data.pages);
+    addBookToLibrary(data.author, data.title, data.pages);
 
     addRemoveClass(addBookForm, 'form-show', 'form-hidden');
     addRemoveClass(pageBody, 'blur-page', 'show-page');
     addBookForm.reset();
 });
+
+function appendMultiple(appendLocation, ...children) {
+    children.forEach((child) => {
+        appendLocation.appendChild(child);
+    });
+};
+
+
+//element and classname to be written as strings
+function createBookDisplay(book) {
+    let bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-div");
+
+    let bookTitle = document.createElement("p");
+    bookTitle.classList.add("book-title");
+    bookTitle.textContent = book.title;
+
+    let bookAuthor = document.createElement("p");
+    bookAuthor.classList.add("book-author");
+    bookAuthor.textContent = book.author;
+
+    let bookPages = document.createElement("p");
+    bookPages.classList.add("book-pages");
+    bookPages.textContent = book.pages;
+
+    mainContentDiv.appendChild(bookDiv);
+    appendMultiple(bookDiv, bookTitle, bookAuthor, bookPages);
+
+};
+
+
 
