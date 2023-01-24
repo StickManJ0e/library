7945f201e37990a06806d8fbae7c6f5492cf6b22
StickManJ0e <121269062+StickManJ0e@users.noreply.github.com>
2023-01-24 16:18:23 +1100

    Add a read property to book object and include a toggle

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let escapeMenuButton = document.querySelector('#escape-menu-button');
 let submitFormButton = document.querySelector('#submit-form-button');
 let myLibrary = [];
 let mainContentDiv = document.querySelector('.main-content');
+let readBook = document.querySelector('#read');
 
 //Shorthand function that removes and adds specific classes for an element
 function addRemoveClass(element, removedClass, addedClass) {
@@ -25,16 +26,17 @@ escapeMenuButton.addEventListener('click', () => {
     addBookForm.reset();
 });
 
-function Book(author, title, pages) {
+function Book(author, title, pages, read) {
     this.author = author;
     this.title = title;
     this.pages = pages;
+    this.read = read;
 };
 
-function addBookToLibrary(author, title, pages) {
-    let book = new Book(`${author}`, `${title}`, pages);
+function addBookToLibrary(author, title, pages, read) {
+    let book = new Book(`${author}`, `${title}`, pages, read);
     myLibrary.push(book);
-    createBookDisplay(book);
+    createBookCard(book);
     console.log(myLibrary);
 };
 
@@ -42,24 +44,40 @@ addBookForm.addEventListener('submit', (event) => {
     //Stop form from actually submitting and turning the data into an object
     event.preventDefault();
     let data = Object.fromEntries(new FormData(event.target).entries());
-    addBookToLibrary(data.author, data.title, data.pages);
+    let readStatus = data.read === 'on' ? true : false;
+    addBookToLibrary(data.author, data.title, data.pages, readStatus);
 
     addRemoveClass(addBookForm, 'form-show', 'form-hidden');
     addRemoveClass(pageBody, 'blur-page', 'show-page');
     addBookForm.reset();
 });
 
+//Append multiple created elements at once to the same div
 function appendMultiple(appendLocation, ...children) {
     children.forEach((child) => {
         appendLocation.appendChild(child);
     });
 };
 
+//Toggle the read/not read for books
+function toggleRead(toggle, book) {
+    toggle.addEventListener('click', () => {
+        if (book.read === true) {
+            addRemoveClass(toggle, 'read', 'not-read');
+            book.read = false;
+            return;
+        }
+        addRemoveClass(toggle, 'not-read', 'read');
+        book.read = true;
+        return;
+    });
+};
+
 
 //element and classname to be written as strings
-function createBookDisplay(book) {
-    let bookDiv = document.createElement("div");
-    bookDiv.classList.add("book-div");
+function createBookCard(book) {
+    let bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
 
     let bookTitle = document.createElement("p");
     bookTitle.classList.add("book-title");
@@ -73,10 +91,14 @@ function createBookDisplay(book) {
     bookPages.classList.add("book-pages");
     bookPages.textContent = book.pages;
 
-    mainContentDiv.appendChild(bookDiv);
-    appendMultiple(bookDiv, bookTitle, bookAuthor, bookPages);
+    let bookRead = document.createElement("input");
+    bookRead.setAttribute("type", "checkbox");
+    bookRead.classList.add("book-read");
+    let readToggle = book.read === true ? bookRead.classList.add("read") : bookRead.classList.add("not-read");
 
+    mainContentDiv.appendChild(bookCard);
+    appendMultiple(bookCard, bookTitle, bookAuthor, bookPages, bookRead);
+    toggleRead(bookRead, book);
 };
-
 
 
